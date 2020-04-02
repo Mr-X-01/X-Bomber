897beb515a9bc560f9c865ffd2a0cd89f3ff1724
Mrx-Anonym <60137544+Mrx-Anonym@users.noreply.github.com>
2020-04-02 19:53:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,26 +18,26 @@ then
 	pkg install dos2unix
 	pip3 install requests
 	pip3 install colorama
-	cp ~/Bomber-Beta /spammer.py /data/data/com.termux/files/usr/bin/Bomber-Beta 
-	dos2unix /data/data/com.termux/files/usr/bin/Bomber-Beta 
-	chmod 777 /data/data/com.termux/files/usr/bin/Bomber-Beta 
-	python3 Bomber-Beta/spammer.py 
+	cp ~/Beta-Bomber /spammer.py /data/data/com.termux/files/usr/bin/Beta-Bomber
+	dos2unix /data/data/com.termux/files/usr/bin/Beta-Bomber
+	chmod 777 /data/data/com.termux/files/usr/bin/Beta-Bomber
+	python3 Beta-Bomber/spammer.py 
 else
 	if [ $numb = "2" ]
 	then
 
 		if [ "$(whoami)" != 'root' ];
 		then
-			echo "You have no rights. Run install.sh with root (sudo sh ~/Bomber-Beta /install.sh)"
+			echo "You have no rights. Run install.sh with root (sudo sh ~/Beta-Bomber/install.sh)"
 			exit
 		else
 			apt install python3 python3-pip
 			pip3 install requests
 			pip3 install colorama
-			cp ~/Bomber-Beta /spammer.py /usr/bin/Bomber-Beta 
-			dos2unix /usr/bin/Bomber-Beta Bomber-Beta 
-			chmod 777 /usr/bin/Bomber-Beta 
-			python3 Bomber-Beta/spammer.py   
+			cp ~/Beta-Bomber /spammer.py /usr/bin/Beta-Bomber
+			dos2unix /usr/bin/Beta-Bomber Beta-Bomber
+			chmod 777 /usr/bin/Beta-Bomber 
+			python3 Beta-Bomber/spammer.py   
 		fi
 	else
 		if [ $numb = "3" ] 
@@ -47,10 +47,10 @@ else
 			apk add dos2unix
 			pip3 install requests
 			pip3 install colorama
-			cp ~/Bomber-Beta /spammer.py /usr/bin/Bomber-Beta 
-			dos2unix /usr/bin/Bomber-Beta 
-			chmod 777 /usr/bin/Bomber-Beta 
-			python3 Bomber-Beta/spammer.py  
+			cp ~/Beta-Bomber /spammer.py /usr/bin/Beta-Bomber
+			dos2unix /usr/bin/Beta-Bomber
+			chmod 777 /usr/bin/Beta-Bomber
+			python3 Beta-Bomber/spammer.py  
 		else
 			echo "Invalid input"
 		fi
