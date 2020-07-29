2448df95e0de6fb2999ad70e21851303094ba9ab
Mr-X-01 <60137544+Mr-X-01@users.noreply.github.com>
2020-07-29 13:37:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ then
 	pip3 install colorama
 	cp ~/X-Bomber /spammer.py /data/data/com.termux/files/usr/bin/X-Bomber 
 	dos2unix /data/data/com.termux/files/usr/bin/X-Bomber 
-	chmod 777 /data/data/com.termux/files/usr/bin/X-Bomber 
+	chmod +x /data/data/com.termux/files/usr/bin/X-Bomber 
 	python3 spammer.py 
 else
 	if [ $numb = "2" ]
